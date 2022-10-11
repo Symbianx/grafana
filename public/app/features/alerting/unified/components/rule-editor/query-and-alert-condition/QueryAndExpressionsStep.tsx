512b89175ddf7d7ad05bd42baa8911e01207dec8
Sonia Aguilar <soniaaguilarpeiron@gmail.com>
2022-10-11 11:27:45 +0200

    update time range for expressions when data sources are updated

--- public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/QueryAndExpressionsStep.tsx
+++ public/app/features/alerting/unified/components/rule-editor/query-and-alert-condition/QueryAndExpressionsStep.tsx
@@ -29,6 +29,7 @@ import {
   updateExpression,
   updateExpressionRefId,
   updateExpressionType,
+  updateExpressionTimeRange,
 } from './reducer';
 
 interface Props {
@@ -117,8 +118,9 @@ export const QueryAndExpressionsStep: FC<Props> = ({ editingExistingRule }) => {
 
   const onChangeQueries = useCallback(
     (updatedQueries: AlertQuery[]) => {
+      console.log('hola');
       dispatch(setDataQueries(updatedQueries));
-
+      dispatch(updateExpressionTimeRange());
       // check if we need to rewire expressions
       updatedQueries.forEach((query, index) => {
         const oldRefId = queries[index].refId;
